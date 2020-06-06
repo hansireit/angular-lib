0bf620351217d00ab55f86b8beeb7411759a0e57
hansi_reit <zandalohd@gmail.com>
2020-06-06 16:59:30 +0200

    Removed angular-basic-dialogs

--- projects/angular-basic-dialogs/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of angular-basic-dialogs
- */
-
-export * from './lib/confirmation-dialog/confirmation-dialog.component';
-export * from './lib/media-item-preview/media-item-preview.component';
-export * from './lib/media-picker/media-picker.component';
-export * from './lib/rename-dialog/rename-dialog.component';
