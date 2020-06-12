ee519f858f3e364fc4d66754182c01cdbb415751
hansi_reit <zandalohd@gmail.com>
2020-06-12 07:16:18 +0200

    Removed data listener from dao

--- projects/angular-base-dao/src/lib/ApiBaseDao.ts
+++ projects/angular-base-dao/src/lib/ApiBaseDao.ts
@@ -1,121 +1,100 @@
 import { Dao } from './Dao';
 import { HttpClient } from '@angular/common/http';
 import { IdentifiableRequestSerializable } from './IdentifiableRequestSerializable';
-import { DataUpdate } from './DataUpdate';
 import { AuthTokenHeader } from './auth/AuthTokenHeader';
 
-export abstract class ApiBaseDao<T extends IdentifiableRequestSerializable> implements Dao<T>{
-    protected observers: DataUpdate<T>[] = [];
-    protected dataList: T[];
-    protected routeUrl: string;
-    protected http: HttpClient;
-    protected authTokenHeader: AuthTokenHeader;
+export abstract class ApiBaseDao<T extends IdentifiableRequestSerializable>
+  implements Dao<T> {
+  protected routeUrl: string;
+  protected http: HttpClient;
+  protected authTokenHeader: AuthTokenHeader;
 
-    constructor(
-        routeUrl: string,
-        http: HttpClient,
-        authService: AuthTokenHeader
-    ) {
-        this.routeUrl = routeUrl;
-        this.http = http;
-        this.authTokenHeader = authService;
-    }
+  constructor(
+    routeUrl: string,
+    http: HttpClient,
+    authService: AuthTokenHeader
+  ) {
+    this.routeUrl = routeUrl;
+    this.http = http;
+    this.authTokenHeader = authService;
+  }
 
-    public list(onUpdate: DataUpdate<T> = null): Promise<T[]> {
-        if (onUpdate) {
-            this.observers.push(onUpdate);
-        }
+  public list(): Promise<T[]> {
+    return new Promise((resolve, reject) => {
+      this.http
+        .get<T[]>(this.routeUrl, { headers: this.authTokenHeader.headers })
+        .subscribe(
+          data => {
+            resolve(data.map(elem => this.createModel(elem)));
+          },
+          error => {
+            reject(error);
+          }
+        );
+    });
+  }
 
-        return new Promise((resolve, reject) => {
-            if (this.dataList) {
-                if (onUpdate) {
-                    onUpdate(this.dataList);
-                }
-                resolve(this.dataList);
+  public create(item: T): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      if (!item) {
+        reject('Could not create item (null)');
+      } else {
+        this.http
+          .post<T>(this.routeUrl, item.toJsonRequestBody(), {
+            headers: this.authTokenHeader.headers
+          })
+          .subscribe(
+            data => {
+              resolve(this.createModel(data));
+            },
+            error => {
+              reject(error);
             }
-            else {
-                this.http.get<T[]>(this.routeUrl, { headers: this.authTokenHeader.headers }).subscribe(
-                    data => {
-                        this.dataList = data.map(value => this.createModel(value));
+          );
+      }
+    });
+  }
 
-                        if (onUpdate) {
-                            onUpdate(this.dataList);
-                        }
-                        resolve(this.dataList);
-                    },
-                    error => {
-                        reject(error);
-                    }
-                );
+  public update(item: T): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      if (!item) {
+        reject('Could not update entity. (null)');
+      } else {
+        this.http
+          .put<T>(this.routeUrl + '/' + item._id, item.toJsonRequestBody(), {
+            headers: this.authTokenHeader.headers
+          })
+          .subscribe(
+            data => {
+              resolve(this.createModel(data));
+            },
+            error => {
+              reject(error);
             }
-        });
-    }
+          );
+      }
+    });
+  }
 
-    public create(item: T): Promise<T> {        
-        return new Promise<T>((resolve, reject) => {
-            if (!item) {
-                reject('Could not create item (null)');
+  public delete(item: T): Promise<T> {
+    return new Promise<T>((resolve, reject) => {
+      if (!item) {
+        reject('Could not delte entity. (null)');
+      } else {
+        this.http
+          .delete<T>(this.routeUrl + '/' + item._id, {
+            headers: this.authTokenHeader.headers
+          })
+          .subscribe(
+            data => {
+              resolve(this.createModel(data));
+            },
+            error => {
+              reject(error);
             }
-            else {
-                this.http.post<T>(this.routeUrl, item.toJsonRequestBody(), { headers: this.authTokenHeader.headers }).subscribe(
-                    data => {
-                        data = this.createModel(data);
-                        this.dataList.push(data);
-                        this.notifyDataChange();
-                        resolve(data);
-                    },
-                    error => {
-                        reject(error);
-                    }
-                );
-            }
-        });
-    }
-
-    public update(item: T): Promise<T> {
-        return new Promise<T>((resolve, reject) => {
-            if (!item) {
-                reject('Could not update game (null)');
-            }
-            else {
-                this.http.put<T>(this.routeUrl + '/' + item._id, item.toJsonRequestBody(), { headers: this.authTokenHeader.headers }).subscribe(
-                    data => {
-                        data = this.createModel(data);
-                        this.notifyDataChange();
-                        resolve(data);
-                    },
-                    error => {
-                        reject(error);
-                    }
-                );
-            }
-        });
-    }
-
-    public delete(item: T): Promise<T> {
-        return new Promise<T>((resolve, reject) => {
-            if (!item) {
-                reject('Could not delte T (null)');
-            }
-            else {
-                this.http.delete<T>(this.routeUrl + '/' + item._id, { headers: this.authTokenHeader.headers }).subscribe(
-                    data => {
-                        data = this.createModel(data);
-                        this.dataList = this.dataList.filter(obj => obj._id !== item._id);
-                        this.notifyDataChange();
-                        resolve(data);
-                    },
-                    error => {
-                        reject(error);
-                    }
-                );
-            }
-        });
-    }
-
-    protected notifyDataChange(): void {
-        this.observers.forEach(observer => observer(this.dataList));
-    }
-
-    protected abstract createModel(propertyData: T) : T;
+          );
+      }
+    });
+  }
+  protected abstract createModel(propertyData: T): T;
 }
