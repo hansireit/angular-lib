7f14920d967c06b15caca35678c3a164da4a913b
hansi_reit <zandalohd@gmail.com>
2020-07-01 09:55:41 +0200

    Replaced identifiable with id

--- projects/angular-base-dao/src/lib/ApiBaseDao.ts
+++ projects/angular-base-dao/src/lib/ApiBaseDao.ts
@@ -2,7 +2,6 @@ import { Dao } from './Dao';
 import { HttpClient } from '@angular/common/http';
 import { IdentifiableRequestSerializable } from './IdentifiableRequestSerializable';
 import { AuthTokenHeader } from './auth/AuthTokenHeader';
-import { Identifiable } from './Identifiable';
 
 export abstract class ApiBaseDao<T extends IdentifiableRequestSerializable>
   implements Dao<T> {
@@ -98,10 +97,10 @@ export abstract class ApiBaseDao<T extends IdentifiableRequestSerializable>
     });
   }
 
-  read(entity: Identifiable): Promise<T> {
+  read(id: string): Promise<T> {
     return new Promise<T>((resolve, reject) => {
       this.http
-        .get<T>(this.routeUrl + '/' + entity._id, {
+        .get<T>(this.routeUrl + '/' + id, {
           headers: this.authTokenHeader.headers
         })
         .subscribe(
--- projects/angular-base-dao/src/lib/Dao.ts
+++ projects/angular-base-dao/src/lib/Dao.ts
@@ -1,9 +1,9 @@
 import { Identifiable } from './Identifiable';
 
 export interface Dao<T> {
-    list(): Promise<T[]>;
-    read(entity: Identifiable): Promise<T>;
-    create(t: T): Promise<T>;
-    delete(t: T): Promise<T>;
-    update(t: T): Promise<T>;
+  list(): Promise<T[]>;
+  read(id: string): Promise<T>;
+  create(t: T): Promise<T>;
+  delete(t: T): Promise<T>;
+  update(t: T): Promise<T>;
 }
