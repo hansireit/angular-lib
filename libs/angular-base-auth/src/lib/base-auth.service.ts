48ef1bece6721c1bdfba221f0421a1b71fc8fe96
hansi_reit <zandalohd@gmail.com>
2023-11-10 11:34:40 +0100

    [FEAT] Prepared angular-base-auth - Added unit-test - Incremented version number

--- libs/angular-base-auth/src/lib/base-auth.service.ts
+++ libs/angular-base-auth/src/lib/base-auth.service.ts
@@ -9,10 +9,11 @@ export abstract class BaseAuthService<T> {
   private readonly _readyLoginState$: Observable<LoginState>;
   private readonly _readyAuthState$: Observable<BaseAuthState<T>>;
   private readonly _user$: Observable<T | null>;
+  private readonly _readyUser$: Observable<T | null>;
   private readonly _isLoggedIn$: Observable<boolean>;
   private readonly defaultLoginRoute = '/auth/login';
 
-  get authState(): Observable<BaseAuthState<T>> {
+  get authState$(): Observable<BaseAuthState<T>> {
     return this._authState$;
   }
 
@@ -32,6 +33,10 @@ export abstract class BaseAuthService<T> {
     return this._user$;
   }
 
+  get readyUser$(): Observable<T | null> {
+    return this._readyUser$;
+  }
+
   get isLoggedIn$(): Observable<boolean> {
     return this._isLoggedIn$;
   }
@@ -42,12 +47,13 @@ export abstract class BaseAuthService<T> {
 
   protected constructor() {
     this._authState$ = this.authStateSubject.asObservable();
+    this._user$ = this._authState$.pipe(map((elem) => elem.user));
     this._loginState$ = this._authState$.pipe(map((elem) => elem.loginState));
     this._isLoggedIn$ = this._loginState$.pipe(map((state) => state === 'logged-in'));
 
     this._readyAuthState$ = this._authState$.pipe(filter((elem) => elem.loginState !== 'pending'));
     this._readyLoginState$ = this._readyAuthState$.pipe(map((elem) => elem.loginState));
-    this._user$ = this._readyAuthState$.pipe(map((elem) => elem.user));
+    this._readyUser$ = this._readyAuthState$.pipe(map((elem) => elem.user));
   }
 
   protected userLoggedIn(user: T): void {
