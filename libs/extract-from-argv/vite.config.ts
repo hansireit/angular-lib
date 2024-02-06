ad600bc6b86e9450357ecdbaf7a4385fc55cfc20
hansi_reit <zandalohd@gmail.com>
2024-02-06 11:42:04 +0100

    [FEAT] Migrated nx workspace to version 18

--- libs/extract-from-argv/vite.config.ts
+++ libs/extract-from-argv/vite.config.ts
@@ -14,12 +14,12 @@ export default defineConfig({
   // },
 
   test: {
+    reporters: ['default'],
     globals: true,
     cache: {
-      dir: '../../node_modules/.vitest',
+      dir: '../../node_modules/.vitest'
     },
     environment: 'node',
-    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-
-  },
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}']
+  }
 });
